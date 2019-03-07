91ed39c1232fe5e5a3c05b0cce71863fe95aef05
Nathan Rebours <nathan@cryptosense.com>
2019-03-07 14:37:47 +0100

    Prefix factory names instead of suffixing them.

--- test/deriver/pp_factory.expected.ml
+++ test/deriver/pp_factory.expected.ml
@@ -7,7 +7,7 @@ module Types :
       int_field: int ;
       string_field: string ;
       other_field: A.B.t }[@@deriving factory]
-    val factory_simple_record :
+    val simple_record_factory :
       ?int_field:int ->
         ?string_field:string -> ?other_field:A.B.t -> unit -> simple_record
     type simple_variant =
@@ -17,31 +17,31 @@ module Types :
       | D of {
       int_field: int ;
       string_field: string } [@@deriving factory]
-    val factory_simple_variant_a : unit -> simple_variant
-    val factory_simple_variant_b : ?tup0:int -> unit -> simple_variant
-    val factory_simple_variant_c :
+    val simple_variant_a_factory : unit -> simple_variant
+    val simple_variant_b_factory : ?tup0:int -> unit -> simple_variant
+    val simple_variant_c_factory :
       ?tup0:int -> ?tup1:string -> unit -> simple_variant
-    val factory_simple_variant_d :
+    val simple_variant_d_factory :
       ?int_field:int -> ?string_field:string -> unit -> simple_variant
     type record_with_options =
       {
       non_optional: int ;
       optional: int option ;
       nested: int option option }[@@deriving factory]
-    val factory_record_with_options :
+    val record_with_options_factory :
       ?non_optional:int ->
         ?optional:int -> ?nested:int option -> unit -> record_with_options
     type 'a parametrized = {
       param: 'a option ;
       non_paramed: string }[@@deriving factory]
-    val factory_parametrized :
+    val parametrized_factory :
       ?param:'a -> ?non_paramed:string -> unit -> 'a parametrized
     type copied = simple_record =
       {
       int_field: int ;
       string_field: string ;
       other_field: A.B.t }[@@deriving factory]
-    val factory_copied :
+    val copied_factory :
       ?int_field:int ->
         ?string_field:string -> ?other_field:A.B.t -> unit -> copied
   end =
@@ -51,7 +51,7 @@ module Types :
       int_field: int ;
       string_field: string ;
       other_field: A.B.t }[@@deriving factory]
-    let factory_simple_record ?(int_field= 0)  ?(string_field= "") 
+    let simple_record_factory ?(int_field= 0)  ?(string_field= "") 
       ?(other_field= A.B.default)  () =
       { int_field; string_field; other_field }
     type simple_variant =
@@ -61,28 +61,28 @@ module Types :
       | D of {
       int_field: int ;
       string_field: string } [@@deriving factory]
-    let factory_simple_variant_a () = A
-    let factory_simple_variant_b ?(tup0= 0)  () = B tup0
-    let factory_simple_variant_c ?(tup0= 0)  ?(tup1= "")  () = C (tup0, tup1)
-    let factory_simple_variant_d ?(int_field= 0)  ?(string_field= "")  () =
+    let simple_variant_a_factory () = A
+    let simple_variant_b_factory ?(tup0= 0)  () = B tup0
+    let simple_variant_c_factory ?(tup0= 0)  ?(tup1= "")  () = C (tup0, tup1)
+    let simple_variant_d_factory ?(int_field= 0)  ?(string_field= "")  () =
       D { int_field; string_field }
     type record_with_options =
       {
       non_optional: int ;
       optional: int option ;
       nested: int option option }[@@deriving factory]
-    let factory_record_with_options ?(non_optional= 0)  ?optional  ?nested 
+    let record_with_options_factory ?(non_optional= 0)  ?optional  ?nested 
       () = { non_optional; optional; nested }
     type 'a parametrized = {
       param: 'a option ;
       non_paramed: string }[@@deriving factory]
-    let factory_parametrized ?param  ?(non_paramed= "")  () =
+    let parametrized_factory ?param  ?(non_paramed= "")  () =
       { param; non_paramed }
     type copied = simple_record =
       {
       int_field: int ;
       string_field: string ;
       other_field: A.B.t }[@@deriving factory]
-    let factory_copied ?(int_field= 0)  ?(string_field= "")  ?(other_field=
+    let copied_factory ?(int_field= 0)  ?(string_field= "")  ?(other_field=
       A.B.default)  () = { int_field; string_field; other_field }
   end 
